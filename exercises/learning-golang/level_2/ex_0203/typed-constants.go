70d7aadc6d9dcddd4ab2416e4ec07bfc1bb0f887
Jeffrey McCrea <duldren@gmail.com>
2018-08-25 13:30:00 -0700

    Add golang video exercise 0203

---
+++ exercises/learning-golang/level_2/ex_0203/typed-constants.go
@@ -0,0 +1,13 @@
+// Create Typed and Untyped constants. Print the values of the constants.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const a int = 43
+	const b = "Hello Dere"
+
+	fmt.Println(b, a)
+}
