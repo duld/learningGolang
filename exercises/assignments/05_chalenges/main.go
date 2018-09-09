bdeeb807d292d4f1f4245b00c3123e90299df556
Jeffrey McCrea <duldren@gmail.com>
2018-09-09 13:53:37 -0700

    Add personal challenge file
    A file that has some commented directions to play around with using
    the packages involved for working with files in go

---
+++ exercises/assignments/05_chalenges/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Howdy")
+}
+
+// read the contents of a directory
+
+// print the contents of a directory
+
+// write a file to the directory
+
+// read the file from the directory
+
+// write over the file in the directory
+
+// append data to the end of the file
+
+// Write a json file
+
+// read the json file back
+
+// handle common errors
+
+// write a simple sign up sheet program.
