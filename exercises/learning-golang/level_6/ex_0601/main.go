b417388515b89689a60983895c7ffd20cae546c3
Jeffrey McCrea <duldren@gmail.com>
2018-09-09 13:52:10 -0700

    Add exercise 0601

---
+++ exercises/learning-golang/level_6/ex_0601/main.go
@@ -0,0 +1,24 @@
+/*
+create a func with the identifier foo that returns an int
+create a func with the identifier bar that returns an int and a string
+call both funcs
+print out their results
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(foo())
+	fmt.Println(bar())
+}
+
+func foo() int {
+	return 45
+}
+
+func bar() (int, string) {
+	return 5, "Philly Cheesesteak"
+}
