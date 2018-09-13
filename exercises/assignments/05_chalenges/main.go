de12736f3465c9484077df8d75de604ed5ed6b86
Jeffrey McCrea <duldren@gmail.com>
2018-09-13 15:54:10 -0700

    Messing with channels

--- exercises/assignments/05_chalenges/main.go
+++ exercises/assignments/05_chalenges/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 2)
+	}
+}
+
 func main() {
-	fmt.Println("Howdy")
+	c := make(chan string)
+
+	go pinger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
 }
 
 // read the contents of a directory
