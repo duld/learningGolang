f6099d3208d304f5f1d1081d40c71e591471678d
Jeffrey McCrea <duldren@gmail.com>
2018-09-20 17:06:14 -0700

    Add exercise parse glob a datastructure

--- exercises/golang-webdev/006-parse-glob-datastructure/main.go
+++ exercises/golang-webdev/006-parse-glob-datastructure/main.go
@@ -1,8 +1,30 @@
-package main
-
 // parse n number of template files using the
 // template.parseglob() method
 
-func main() {
+package main
 
+import (
+	"html/template"
+	"os"
+)
+
+type Bookshelf struct {
+	Books []string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("*.gohtml"))
+}
+
+func main() {
+	books := Bookshelf{
+		Books: []string{"The Great Gatsby",
+			"War and Peace",
+			"Great Expectations"},
+	}
+
+	// execute index.html
+	tpl.ExecuteTemplate(os.Stdout, "index.gohtml", books)
 }
