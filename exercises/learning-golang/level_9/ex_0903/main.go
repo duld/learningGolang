6ecda16f4e23aa7cf7c4ffaf7346cd3d309a56c4
Jeffrey McCrea <duldren@gmail.com>
2018-09-23 13:38:14 -0700

    Add exercise 0903 waitgroups

---
+++ exercises/learning-golang/level_9/ex_0903/main.go
@@ -0,0 +1,48 @@
+/*
+Using goroutines, create an incrementer program
+  have a variable to hold the incrementer value
+  launch a bunch of goroutines
+  each goroutine should
+    read the incrementer value
+    store it in a new variable
+		yield the processor with runtime.Gosched()
+		increment the new variable
+		write the value in the new variable back to the incrementer variable
+use waitgroups to wait for all of your goroutines to finish
+the above will create a race condition.
+Prove that it is a race condition by using the -race flag
+*/
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var mx sync.Mutex
+
+func main() {
+	r := rand.Intn(30)
+	total := 0
+	loopLimit := 200
+	wg.Add(loopLimit)
+
+	for i := 0; i < loopLimit; i++ {
+		go func() {
+			mx.Lock()
+			loc := total
+			time.Sleep(time.Duration(r) * time.Millisecond)
+			loc++
+			total = loc
+			mx.Unlock()
+			wg.Done()
+		}()
+	}
+
+	// wait for our go routines to finish
+	wg.Wait()
+	fmt.Println(total)
+}
