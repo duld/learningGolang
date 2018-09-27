e880983b65d092340e184f4f0cd6e3d095af74f5
Jeffrey McCrea <duldren@gmail.com>
2018-09-26 21:24:40 -0700

    Add wk7 examples

---
+++ exercises/playground/template-data-to-file/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"os"
+)
+
+var tpl *template.Template
+
+// load in the templates
+func init() {
+	tpl = template.Must(template.ParseGlob("./templates/*.gohtml"))
+}
+
+func main() {
+
+	// create two files
+	// f, err := os.Create("authors.html")
+	// if err != nil {
+	// 	log.Fatal("There was a problem creating the authors file", err)
+	// }
+	// defer f.Close()
+
+	// f2, err := os.Create("books.html")
+	// if err != nil {
+	// 	log.Fatal("There was a problem creating the books file", err)
+	// }
+	// defer f2.Close()
+
+	// // write to the files
+	// err = tpl.ExecuteTemplate(f, "authors.gohtml", nil)
+	// if err != nil {
+	// 	log.Fatal("Problem creating html file from template", err)
+	// }
+
+	// err = tpl.ExecuteTemplate(f2, "books.gohtml", nil)
+	// if err != nil {
+	// 	log.Fatal("Problem creating html file from template", err)
+	// }
+	xtemps := []string{"authors", "books"}
+	for _, v := range xtemps {
+		templateToFile(v, []string{"Ocean sand is dumb and so are birds", "My brother the mouse cursor", "Papa was a rolling stone and I need surgery"})
+	}
+}
+
+func templateToFile(templateName string, data interface{}) {
+	f, err := os.Create(templateName + ".html")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("There was a problem creating the %v file from template", templateName), err)
+	}
+	defer f.Close()
+
+	err = tpl.ExecuteTemplate(f, templateName+".gohtml", data)
+	if err != nil {
+		log.Fatal("Problem creating html file from template", err)
+	}
+}
